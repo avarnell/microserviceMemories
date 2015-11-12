c6512aecd6b052e12601e0892a641961093bf475
Andrew <avarnell@ago.org>
2015-11-12 16:14:47 -0700

    added get all route

--- readme.md
+++ readme.md
@@ -2,6 +2,16 @@
 https://github.com/avarnell/microserviceMemories
 
 *Description
+To use send an object with {
+  "data": {
+    "type": "memory",
+    "attributes": {
+      "old_days": "string",
+      "these_days": "string",
+      "year": 0
+    }
+
+Where string and 0 represent your data.
 
 *goes to this URL:
 http://g12-varnell-andrew-memories.cfapps.io/
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var conString = process.env.DBPATHProd;
-
-/* GET home page. */
+var conString = process.env.DBPATHDev;
 
 router.post('/api/v1/memories', function(req,res,next){
     pg.connect(conString, function(err, client, done){
-    client.query('INSERT INTO memories(old_days, these_days, year) VALUES($1, $2, $3) returning id',[req.body.old_days, req.body.these_days, req.body.year], function(err, result){
+      client.query('INSERT INTO memories(old_days, these_days, year) VALUES($1, $2, $3) returning id',[req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result){
       done()
       res.send('Memory Created')
     })
   })
 })
 
+router.get('/api/v1/memories', function(req,res,next){
+  pg.connect(conString, function(err,client,done){
+    client.query('SELECT * from memories', function(err, result){
+      done()
+
+      var results = { "links": {},
+        "data": [] }
+
+      result.rows.forEach(function(row){
+        var memory = {
+          "type": "memory",
+          "id": row.id,
+          "attributes": {
+            "old_days": row.old_days,
+            "these_days": row.these_days,
+            "year": row.year
+          },
+          "links": {}
+        }
+        results.data.push(memory)
+      })
+      res.json(results)
+    })
+  })
+})
 
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
