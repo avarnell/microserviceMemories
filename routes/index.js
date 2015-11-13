924997c5215bf2859072016b5d5d2dd202d795e2
Andrew <avarnell@ago.org>
2015-11-13 08:50:50 -0700

    added get by years route, tested

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var conString = process.env.DBPATHDev;
+var conString = process.env.DATABASE_URL;
 
 router.post('/api/v1/memories', function(req,res,next){
     pg.connect(conString, function(err, client, done){
-      client.query('INSERT INTO memories(old_days, these_days, year) VALUES($1, $2, $3) returning id',[req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result){
+      client.query('INSERT INTO memories(old_days, these_days, year) VALUES($1, $2, $3) returning id;', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result){
       done()
-      res.send('Memory Created')
+      res.json(result.rows)
     })
   })
 })
@@ -16,9 +16,31 @@ router.get('/api/v1/memories', function(req,res,next){
   pg.connect(conString, function(err,client,done){
     client.query('SELECT * from memories', function(err, result){
       done()
-
       var results = { "links": {},
         "data": [] }
+      result.rows.forEach(function(row){
+        var memory = {
+          "type": "memory",
+          "id": row.id,
+          "attributes": {
+            "old_days": row.old_days,
+            "these_days": row.these_days,
+            "year": Number(row.year)
+          },
+          "links": {}
+        }
+        results.data.push(memory)
+      })
+      res.json(results)
+    })
+  })
+})
+
+router.get('/api/v1/memories/:year', function(req,res,next){
+  var searchYear = req.params.year
+  pg.connect(conString, function(err,client,done){
+    client.query('SELECT * FROM memories WHERE year = $1', [searchYear], function(err,result){
+      var results = { "links": {}, "data": [] }
 
       result.rows.forEach(function(row){
         var memory = {
@@ -27,7 +49,7 @@ router.get('/api/v1/memories', function(req,res,next){
           "attributes": {
             "old_days": row.old_days,
             "these_days": row.these_days,
-            "year": row.year
+            "year": Number(row.year)
           },
           "links": {}
         }
